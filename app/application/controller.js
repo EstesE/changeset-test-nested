2352b2baf178394984e696f7cf935503fa5c36ce
Eric Estes <eric_estes@aspensquare.com>
2018-05-21 14:43:39 -0400

    Updated to use changeset.rollback and added changeset values in labels.

--- app/application/controller.js
+++ app/application/controller.js
@@ -3,7 +3,10 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
         restore(changeset) {
-            changeset.rollback(this.snapshot);
+            // changeset.rollback(this.snapshot);
+            debugger;
+            // changeset.restore(this.snapshot);
+            changeset.rollback();
         }
     }
 });
